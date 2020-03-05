24b0a26aa1497930940120b6e2ae88980bcec435
hyzsky <hyzsky@qq.com>
2020-03-06 00:11:47 +0800

    初使化计数器项目的基本结构

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div> -->
+  <div>
+    <!-- <div>hello world.</div> -->
+    <my-addition></my-addition>
+    <p>--------------</p>
+    <my-subtraction></my-subtraction>
+  </div>
+</template>
+
+<script>
+import Addition from './components/Addition.vue'
+import Subtraction from './components/Subtraction.vue'
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+    'my-addition': Addition,
+    'my-subtraction': Subtraction
+  }
+}
+</script>
+<!-- /* <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+ */ -->
